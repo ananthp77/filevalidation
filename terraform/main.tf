f25f46b4bf792d7c125e3b4827e966469c3103cf
Ananth <ananthp77@gmail.com>
2025-04-10 21:04:39 +0100

    'changes'

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,39 +1,7 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-resource "google_storage_bucket" "csv_bucket" {
-  name     = "${var.project_id}-csv-bucket"
-  location = var.region
-}
-
-resource "google_bigquery_dataset" "data_dataset" {
-  dataset_id = "data_pipeline"
-  location   = var.region
-}
-
-resource "google_bigquery_table" "staging_table" {
-  dataset_id = google_bigquery_dataset.data_dataset.dataset_id
-  table_id   = "staging"
-  schema     = file("${path.module}/bq/staging_schema.json")
-}
-
-resource "google_bigquery_table" "final_table" {
-  dataset_id = google_bigquery_dataset.data_dataset.dataset_id
-  table_id   = "final"
-  schema     = file("${path.module}/bq/final_schema.json")
-}
-
-resource "google_storage_bucket_object" "cloudfunction_zip" {
-  name   = "cloudfunction-source.zip"
-  bucket = google_storage_bucket.csv_bucket.name
-  source = "${path.module}/cloudfunction/cloudfunction-source.zip"
-}
-
 resource "google_cloudfunctions2_function" "validator_fn" {
   name     = "validate-csv-function"
   location = var.region
+
   build_config {
     runtime     = "python311"
     entry_point = "validate_csv"
@@ -51,17 +19,11 @@ resource "google_cloudfunctions2_function" "validator_fn" {
   }
 
   event_trigger {
-  event_type     = "google.cloud.storage.object.v1.finalized"
-  trigger_region = var.region
-  event_filters {
-    attribute = "bucket"
-    value     = google_storage_bucket.csv_bucket.name
+    event_type     = "google.cloud.storage.object.v1.finalized"
+    trigger_region = var.region
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.csv_bucket.name
+    }
   }
-}
-
-resource "google_workflows_workflow" "csv_pipeline" {
-  name            = "csv-pipeline"
-  region          = var.region
-  source_contents = file("${path.module}/workflows/csv_pipeline.yaml")
-
-}
+} # <-- This closing brace was missing
